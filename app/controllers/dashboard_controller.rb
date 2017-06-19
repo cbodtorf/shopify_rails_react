6f3439910bd822dd41876d59ac8bf474856b9416
Caleb Bodtorf <caleb@e4consults.com>
2017-06-19 17:58:03 -0400

    pushing through on the dashboard, I've got counts for non date related fields, but I need to have orders with note attributes to filter through.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,34 @@
 class DashboardController < ShopifyApp::AuthenticatedController
   def index
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
+    # Shopify requires time to be iso8601 format
+    # Order Information 7 day range (limit for which orders )
+    # TODO: make sure this range is right,
+    t = Time.now
+    t8601 = t.iso8601
+    sixDaysAgo = (t - 6.day).iso8601
+    Rails.logger.debug("Time: #{t8601.inspect}")
+    Rails.logger.debug("Time - 1: #{sixDaysAgo.inspect}")
+    @orders = ShopifyAPI::Order.find(:all, params: { created_at_min: sixDaysAgo })
+
+    @allOrders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
+    Rails.logger.debug("orders: #{@orders.inspect}")
+
+
+    # Subscriber Count (tagged with Active Subscriber)
+    # http://support.rechargepayments.com/article/191-shopify-order-tags
+    customers = ShopifyAPI::Customer.find(:all)
+    activeSubscribers = customers.select do |c|
+      c.attributes[:tags].split(', ').include?('Active Subscriber')
+    end
+    @activeSubscriberCount = activeSubscribers.count
+
+    # Customer Count:
+    @customerCount = ShopifyAPI::Customer.count
+
+    # Bundle Information
     @bundles = ShopifyAPI::Product.find(:all, params: { product_type: 'bundle' })
+
+    # Product Information
+    @productCount = ShopifyAPI::Product.count
   end
 end
--- client/app/bundles/Dashboard/components/Dashboard.jsx
+++ client/app/bundles/Dashboard/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Page, Card, Banner, Tabs, Layout, Stack, Button, ButtonGroup} from '@shopify/polaris';
+import {Page, Card, Banner, Tabs, Layout, Stack, Button, ButtonGroup, Heading, Link} from '@shopify/polaris';
 import {EmbeddedApp} from '@shopify/polaris/embedded';
 import Order from './Order';
 
@@ -33,32 +33,62 @@ class Dashboard extends React.Component {
   render() {
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     const weekNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    let orders = this.state.orders.map(order => {
-      return (
-        <Order key={order.id} orderData={order} shopOrigin={this.props.shopOrigin}/>
-      );
-    })
+
     let dates = this.state.dates.map((date, i) => {
       var m = monthNames[date.getMonth()]
       var w = weekNames[date.getDay()]
       var d = date.getDate()
       return (
-        <Card key={i} sectioned>
-          <div className="date">
-            <h5>{m}</h5>
-            <h5>{d}</h5>
-            <h5>{w}</h5>
-          </div>
-          <div className="revenue">
-            <h5>$24,003</h5>
-          </div>
+        <div key={i} className="dashboard-card-primary">
+          <Card sectioned subdued={i != 0 ? true : false} >
+            <div className="date">
+              <h5>{m}</h5>
+              <h5>{d}</h5>
+              <h5>{w}</h5>
+            </div>
+            <div className="revenue">
+              <h5>$24,003</h5>
+            </div>
 
+            <div className="delivery-wrapper morning">
+              <Card sectioned title="Morning">
+                <div className="delivery-count">
+                  <h5>10</h5>
+                  <Link url=''>Orders</Link>
+                </div>
 
-          <ButtonGroup segmented>
-            <Button fullWidth icon='view'></Button>
-            <Button fullWidth icon='notes'></Button>
-          </ButtonGroup>
-        </Card>
+                <ButtonGroup segmented>
+                  <Button fullWidth icon="view"></Button>
+                  <Button fullWidth icon="notes"></Button>
+                </ButtonGroup>
+              </Card>
+            </div>
+
+            <div className="delivery-wrapper afternoon">
+              <Card sectioned title="Afternoon">
+                <div className="delivery-count">
+                  <h5>8</h5>
+                  <Link url=''>Orders</Link>
+                </div>
+
+                <ButtonGroup segmented>
+                  <Button fullWidth icon="view"></Button>
+                  <Button fullWidth icon="notes"></Button>
+                </ButtonGroup>
+              </Card>
+            </div>
+
+            <div className="delivery-wrapper pickup">
+              <Card sectioned title="Pickups">
+                <div className="delivery-count">
+                  <h5>2</h5>
+                  <Link url=''>Orders</Link>
+                </div>
+
+              </Card>
+            </div>
+          </Card>
+        </div>
       );
     })
 
@@ -68,37 +98,40 @@ class Dashboard extends React.Component {
         shopOrigin={this.props.shopOrigin}
       >
         <Page title="Dashboard">
-        <Tabs
-          selected={0}
-          fitted
-          tabs={[
-            {
-              id: 'dashboard',
-              title: 'Dashboard',
-              panelID: 'dashboard',
-              url: '/dashboard',
-            },
-            {
-              id: 'rates',
-              title: 'Rates',
-              panelID: 'rates',
-              url: '/rates',
-            },
-            {
-              id: 'bundles',
-              title: 'Bundles',
-              panelID: 'bundles',
-              url: `/bundle?id=${this.props.bundles[0].id}`,
-            },
-            {
-              id: 'settings',
-              title: 'Settings',
-              panelID: 'settings',
-              url: '/settings',
-            },
-          ]}
-        />
           <Layout>
+            <Layout.Section>
+              <Tabs
+                selected={0}
+                fitted
+                tabs={[
+                  {
+                    id: 'dashboard',
+                    title: 'Dashboard',
+                    panelID: 'dashboard',
+                    url: '/dashboard',
+                  },
+                  {
+                    id: 'rates',
+                    title: 'Rates',
+                    panelID: 'rates',
+                    url: '/rates',
+                  },
+                  {
+                    id: 'bundles',
+                    title: 'Bundles',
+                    panelID: 'bundles',
+                    url: `/bundle?id=${this.props.bundles[0].id}`,
+                  },
+                  {
+                    id: 'settings',
+                    title: 'Settings',
+                    panelID: 'settings',
+                    url: '/settings',
+                  },
+                ]}
+              />
+            </Layout.Section>
+
             <Layout.Section>
               <Stack
                 alignment="fill"
@@ -108,11 +141,21 @@ class Dashboard extends React.Component {
                 { dates }
               </Stack>
             </Layout.Section>
+            <Layout.Section>
+              <div className="counts">
+                <Heading>Counts</Heading>
+                <Stack
+                  spacing="loose"
+                  alignment="center"
+                  distribution="center"
+                  >
+                  <Card sectioned title="Active Subscriptions" ><h5 className="count-content">{ this.props.activeSubscriberCount }</h5></Card>
+                  <Card sectioned title="Total Customers" ><h5 className="count-content">{ this.props.customerCount }</h5></Card>
+                  <Card sectioned title="Number of Products" ><h5 className="count-content">{ this.props.productCount }</h5></Card>
+                </Stack>
+              </div>
+            </Layout.Section>
           </Layout>
-          <Card
-          >
-          { orders }
-          </Card>
         </Page>
       </EmbeddedApp>
     );
