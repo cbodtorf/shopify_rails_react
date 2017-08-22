c8e2ccd2bd10afcd9b990d96c1e3cc40c4bf38a8
Cbodtorf <cbodtorf@gmail.com>
2017-08-22 15:42:39 -0400

    trying to fix webpack assets.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -47,7 +47,7 @@ const config = {
           loader: 'file-loader',
           options: {
             name: '[name][md5:hash].[ext]', // Name of bundled asset
-            outputPath: 'webpack-assets/', // Output location for assets. Final: `app/assets/webpack/webpack-assets/`
+            outputPath: '/webpack-assets/', // Output location for assets. Final: `app/assets/webpack/webpack-assets/`
             publicPath: '/assets' // Endpoint asset can be found at on Rails server
           }
         }
