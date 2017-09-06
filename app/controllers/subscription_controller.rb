be8119fa4d8bb3c68171c8f4f7f35d4bdf90ec25
Caleb Bodtorf <caleb@e4consults.com>
2017-09-06 17:17:46 -0400

    reworked backend and how data was being requested from recharge, much faster! Data formatted a bit differently so I reworked the frontend and added product/first name/last name search. Also the ability to show product.

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -1,9 +1,11 @@
 class SubscriptionController < ShopifyApp::AuthenticatedController
   def index
     # Retrieves Upcoming Active Subscriptions and attaches customer data to the hash.
-    @rechargeSubscriptions = self.getRechargeData("https://api.rechargeapps.com/subscriptions?status=ACTIVE&limit=250")["subscriptions"].each do |sub|
-      sub["customer"] = self.getRechargeData("https://api.rechargeapps.com/customers/#{sub["customer_id"]}")["customer"]
-    end
+    # @rechargeSubscriptions = self.getRechargeData("https://api.rechargeapps.com/subscriptions?status=ACTIVE&limit=250")["subscriptions"].each do |sub|
+    #   sub["customer"] = self.getRechargeData("https://api.rechargeapps.com/customers/#{sub["customer_id"]}")["customer"]
+    # end
+
+    @rechargeSubscriptions = self.getRechargeData("https://api.rechargeapps.com/charges/?status=QUEUED&limit=250")['charges']
 
     # Subscriber Count (tagged with Active Subscriber)
     # http://support.rechargepayments.com/article/191-shopify-order-tags
@@ -12,7 +14,7 @@ class SubscriptionController < ShopifyApp::AuthenticatedController
       c.attributes[:tags].split(', ').include?('Active Subscriber')
     end
     @activeSubscriberCount = activeSubscribers.count
-    
+
 
   end
 
--- client/app/bundles/Subscription/components/Subscription.jsx
+++ client/app/bundles/Subscription/components/Subscription.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Page, Card, Banner, Tabs, Layout, Stack, Button, ButtonGroup, Heading, Subheading, Link, Icon, Tooltip, ResourceList, Pagination} from '@shopify/polaris';
+import {Page, Card, Banner, Tabs, Badge, Layout, Stack, Button, ButtonGroup, Heading, Subheading, Link, Icon, Tooltip, ResourceList, Pagination} from '@shopify/polaris';
 import {EmbeddedApp, Modal} from '@shopify/polaris/embedded';
 import Navigation from '../../Global/components/Navigation';
 import bambooIcon from 'assets/green-square.jpg';
 
 import SearchBar, {createFilter} from '../../Global/components/SearchBar';
 
-const KEYS_TO_FILTERS = ['product_title', 'customer.first_name', 'customer.last_name']
+const KEYS_TO_FILTERS = ['line_items.title', 'first_name', 'last_name']
 
 class Subscription extends React.Component {
   constructor(props) {
@@ -15,7 +15,8 @@ class Subscription extends React.Component {
     this.state = {
       modalOpen: false,
       modalUrl: '',
-      searchTerm: ''
+      searchTerm: '',
+      showProduct: {}
     }
   }
 
@@ -24,29 +25,53 @@ class Subscription extends React.Component {
   }
 
   render() {
-
+    let urlBase = 'https://bamboojuices.myshopify.com/admin/apps/shopify-recurring-payments/'
     let subscriptionList = this.props.subscriptions.filter(
       createFilter(this.state.searchTerm, KEYS_TO_FILTERS)
     ).map(sub => {
-      let urlBase = 'https://bamboojuices.myshopify.com/admin/apps/shopify-recurring-payments/'
+      let productBadges = sub.line_items.map((item, i) => {
+          return (
+            <Badge key={i} status="info">{item.title.replace('Auto renew', '') + ' x' + item.quantity}</Badge>
+          )
+        })
       return (
-        <tr key={ sub.id }>
-          <td><Link external="true" url={ `${urlBase}addresses/${sub.address_id}` }>#{ sub.id }</Link></td>
-          <td><Link external="true" url={ `${urlBase}customer/${sub.customer_id}/subscription/${sub.id}` }>{ sub.customer.first_name + ' ' + sub.customer.last_name }</Link></td>
-          <td>{ new Date(sub.next_charge_scheduled_at).toLocaleDateString() }</td>
-          <td>{ new Date(new Date(sub.next_charge_scheduled_at).setDate(new Date(sub.next_charge_scheduled_at).getDate() + 1)).toLocaleDateString() }</td>
-          <td>${ sub.price.toFixed(2) }</td>
-          <td><Link external="true" onClick={() => {
-              this.setState({ modalOpen: true,
-                modalUrl: `http://bamboojuices.myshopify.com/tools/recurring/customers/${sub.customer.hash}/subscriptions/`
-              })
-            }}>Edit</Link></td>
-          <td><Link external="true" onClick={ () => {
-              this.setState({ modalOpen: true,
-                modalUrl: `http://bamboojuices.myshopify.com/tools/recurring/customers/${sub.customer.hash}/delivery_schedule/`
-              })
-            }}>Edit</Link></td>
-        </tr>
+          <tbody key={ sub.id }>
+            <tr>
+              <td><Link external="true" url={ `${urlBase}addresses/${sub.address_id}` }>#{ sub.id }</Link></td>
+              <td><Link external="true" url={ `${urlBase}customer/${sub.customer_id}/subscription/${sub.id}` }>{ sub.first_name + ' ' + sub.last_name }</Link></td>
+              <td>{ new Date(sub.scheduled_at).toLocaleDateString() }</td>
+              <td>{ new Date(new Date(sub.scheduled_at).setDate(new Date(sub.scheduled_at).getDate() + 1)).toLocaleDateString() }</td>
+              <td>${ sub.total_price }</td>
+
+              <td><Link external="true" onClick={() => {
+                  this.setState({ modalOpen: true,
+                    modalUrl: `http://bamboojuices.myshopify.com/tools/recurring/customers/${sub.customer_hash}/subscriptions/`
+                  })
+                }}>Edit</Link></td>
+
+              <td><Link onClick={ () => {
+                  this.setState({ modalOpen: true,
+                    modalUrl: `http://bamboojuices.myshopify.com/tools/recurring/customers/${sub.customer_hash}/delivery_schedule/`
+                  })
+                }}>Edit</Link></td>
+
+              <td><Link onClick={ () => {
+                  let showProduct = this.state.showProduct || {}
+                  showProduct[sub.id] === true ? showProduct[sub.id] = false : showProduct[sub.id] = true
+                  this.setState({
+                    showProduct: showProduct
+                  })
+                }}>{ this.state.showProduct[sub.id] === true ? 'Hide' : 'Show' }</Link></td>
+            </tr>
+
+            <tr className={`product ${this.state.showProduct[sub.id] === true ? 'show-product' : 'hide-product'}`}>
+              <th colSpan="8">
+                <Stack spacing="tight" distribution="leading">
+                  { productBadges }
+                </Stack>
+              </th>
+            </tr>
+          </tbody>
       )
     })
 
@@ -67,7 +92,6 @@ class Subscription extends React.Component {
       >
         <Page
           icon={ bambooIcon }
-          primaryAction={{ content: 'Back', onAction: () => { window.location.href = '/dashboard' } }}
           >
           <Layout>
             <Layout.Section>
@@ -92,11 +116,10 @@ class Subscription extends React.Component {
                           <th>Total</th>
                           <th>Subscription</th>
                           <th>Schedule</th>
+                          <th>Product</th>
                         </tr>
                       </thead>
-                      <tbody>
                         { subscriptionList }
-                      </tbody>
                     </table>
                 </Card>
             </Layout.Section>
