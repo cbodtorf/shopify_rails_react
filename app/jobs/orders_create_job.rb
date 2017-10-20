660be0585fc94a19d13375c9ea467b15dc4622c5
Caleb Bodtorf <caleb@e4consults.com>
2017-10-20 12:07:55 -0400

    fixed how date is formatted for recurring subs. in order create job.

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -8,14 +8,15 @@ class OrdersCreateJob < ApplicationJob
       # Re-adjusting the delivery date on subsequent subscription orders.
       # Delivery Date == Charge Date + 1
       if webhook[:tags].split(', ').include?('Subscription Recurring Order')
-        delivery_date = Date.parse(webhook[:created_at]) + 1
+        Rails.logger.info("[webhook[:created_at]]: #{webhook[:created_at].inspect}")
+        delivery_date = (Date.parse(webhook[:created_at]) + 1)
 
         recurringSubscriptionOrder = ShopifyAPI::Order.find(webhook[:id])
         Rails.logger.info("[Recurring Sub Order]: #{recurringSubscriptionOrder.attributes[:note_attributes].inspect}")
         recurringSubscriptionOrder.attributes[:note_attributes].each do |note|
           if note.attributes[:name] == "delivery_date"
-            note.attributes[:value] = delivery_date
-            Rails.logger.info("[change delivery date?]: #{note.attributes[:value].inspect} -vs- #{delivery_date}")
+            note.attributes[:value] = delivery_date.readable_inspect
+            Rails.logger.info("[change delivery date?]: #{note.attributes[:value].inspect} -vs- #{delivery_date.readable_inspect}")
           end
         end
         Rails.logger.info("[Recurring delivery date]: #{recurringSubscriptionOrder.attributes[:note_attributes].inspect}")
