bc98e1a04f9fd17fe8eacbbae5b994aa569916a4
Caleb Bodtorf <caleb@e4consults.com>
2017-10-27 17:43:31 -0400

    fixed bug where recurring subs were not being saved.

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -9,18 +9,23 @@ class OrdersCreateJob < ApplicationJob
       order = ShopifyAPI::Order.find(webhook[:id])
       rates = shop.rates.select("id, title")
       saveOrder = false
+      shippingLine = webhook[:shipping_lines].first[:title]
 
       if webhook[:tags].split(', ').include?('Subscription')
         # TODO: change rate id note attribute if order shipping line is different
         # TODO: this is temporary fix, should prevent this from happening
         subscriptionOrder = order
         Rails.logger.info("[subscriptionOrder]: #{subscriptionOrder.inspect}")
+        Rails.logger.info("[shippingLine]: #{shippingLine.inspect}")
+
 
         subscriptionOrder.attributes[:note_attributes].each do |note|
+          Rails.logger.info("[note?]: #{note.inspect}")
           if note.attributes[:name] == "rate_id"
-            if rates.find(5)[:title] != webhook[:shipping_lines].attributes[:title]
-              note.attributes[:value] = rates.find_by(title: webhook[:shipping_lines].attributes[:title])[:id]
+            Rails.logger.info("[rate match]: #{rates.find(note.attributes[:value].to_i)[:title]} != #{shippingLine} ? #{rates.find(note.attributes[:value].to_i)[:title] != shippingLine}")
+            if rates.find(note.attributes[:value].to_i)[:title] != shippingLine
               Rails.logger.info("[change shipping rate?]: #{note.attributes[:value].inspect}")
+              note.attributes[:value] = rates.find_by(title: shippingLine)[:id].to_i
               saveOrder = true
             end
           end
@@ -29,6 +34,7 @@ class OrdersCreateJob < ApplicationJob
 
         # Re-adjusting the delivery date on subsequent subscription orders.
         # Delivery Date == Charge Date + 1
+        Rails.logger.info("[recurring?]: #{webhook[:tags].split(', ').include?('Subscription Recurring Order').inspect}")
         if webhook[:tags].split(', ').include?('Subscription Recurring Order')
           Rails.logger.info("[webhook[:created_at]]: #{webhook[:created_at].inspect}")
           delivery_date = (Date.parse(webhook[:created_at]) + 1)
@@ -45,6 +51,7 @@ class OrdersCreateJob < ApplicationJob
         end
       end
 
+      Rails.logger.info("[saveOrder?]: #{saveOrder.inspect}")
       if saveOrder
         subscriptionOrder.save
       end
