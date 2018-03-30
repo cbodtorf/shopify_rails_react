0ae6a46a99f4739202a1dd84a93a10ecd83dad3f
Caleb Bodtorf <caleb@e4consults.com>
2018-03-30 16:08:58 -0400

    send recharge address/charge/sub info to portal for customization.

--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -363,9 +363,31 @@ class AppProxyController < ApplicationController
     shop = Shop.find_by(shopify_domain: params[:shop])
     session = ShopifyApp::SessionRepository.retrieve(shop.id)
     ShopifyAPI::Base.activate_session(session)
+    subscriptions = []
+    params[:address_ids].split(',').each do |id|
+      address_id = id
+      address_1 = shop.getRechargeData("https://api.rechargeapps.com/addresses/#{id}")["address"]["address1"]
+      # min_by will grab to earliest upcoming charge to avoid dates
+      next_charge_scheduled_at = shop.getRechargeData("https://api.rechargeapps.com/subscriptions?address_id=#{id}&status=ACTIVE")["subscriptions"]
+      Rails.logger.debug("[charge all #{next_charge_scheduled_at.inspect}")
+      Rails.logger.debug("[charge size #{next_charge_scheduled_at.size}")
+
+      if next_charge_scheduled_at.size >= 1
+        next_charge_scheduled_at = next_charge_scheduled_at.min_by do |sub|
+          Rails.logger.debug("[charge****] #{sub.inspect}")
+          Date.parse(sub["next_charge_scheduled_at"])
+        end["next_charge_scheduled_at"]
+      else
+        Rails.logger.debug("[next charge none] #{next_charge_scheduled_at.inspect}")
+        next_charge_scheduled_at = "please enter date"
+      end
+
+      subscriptions.push({ address_id: address_id, address_1: address_1, next_charge_scheduled_at: next_charge_scheduled_at })
+    end
+    Rails.logger.debug("[subscriptions] #{subscriptions.inspect}")
 
     blackout_dates = shop.blackout_dates.pluck_to_hash(:title, :blackout_date)
-    render json: { blackoutDates: blackout_dates } , status: 200
+    render json: { blackoutDates: blackout_dates, subscriptions: subscriptions } , status: 200
   end
 
   def update_subscription_bundle_props
